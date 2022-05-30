d3383f7e1c2e793e0ce5c56f99f71b0b365bb94a
touille3222 <wush3222@gmail.com>
2022-05-30 23:49:44 +0700

    add gallery button, and add intent explicit and implicit

--- MD/MedsApp/app/src/main/java/com/capstone/medsapp/MainActivity.kt
+++ MD/MedsApp/app/src/main/java/com/capstone/medsapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.capstone.medsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.medsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupAction()
+    }
+
+    private fun setupAction(){
+        binding.btnStart.setOnClickListener {
+            val intent = Intent(this@MainActivity, StartActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- MD/MedsApp/app/src/main/java/com/capstone/medsapp/StartActivity.kt
+++ MD/MedsApp/app/src/main/java/com/capstone/medsapp/StartActivity.kt
@@ -2,10 +2,25 @@ package com.capstone.medsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.medsapp.databinding.ActivityStartBinding
+
+import android.content.Intent
 
 class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupAction()
+    }
+
+    private fun setupAction(){
+        binding.btnCamera.setOnClickListener {
+            val intent = Intent("android.media.action.IMAGE_CAPTURE")
+            startActivity(intent)
+        }
     }
 }
